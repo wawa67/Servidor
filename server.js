1897fa3e149807d3625850e1213b6310d9c89208
Wallace de Souza <wallace.silvadesouza67@gmail.com>
2020-01-28 14:17:25 -0300

    update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = '3000';
 const server = http.createServer((req,res)=>{
     res.statusCode = 200;
     res.setHeader('Content-Type','text/plain');
-    res.end('Meu primeiro servidor');
+    res.end('Servidor Executando');
 });
 
 server.listen(port,host, ()=>{
